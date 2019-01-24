dfd2950fcc83700eebc2c3c477507c645528e220
Wencke <wencke.nuyt@student.howest.be>
2019-01-24 12:36:00 +0100

    images home

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -16,79 +16,28 @@
     <p>Humor, acrobatie, stuntwerk van de bovenste plank uit ruim 15 verschillende landen! Bovenal geniet u van dit festival volledig GRATIS.</p>
     <a class="cta" href="index.php?page=programma">Programma</a>
   </div>
-
-  <!-- <div class="carouselbox"> -->
-    <!-- <div class="buttons">
-      <button class="prev">
-        ◀ <span class="offscreen">Previous</span>
-      </button>
-      <button class="next">
-        <span class="offscreen">Next</span> ▶
-      </button>
-    </div> -->
-    <ul class="content">
-        <li class="content__li">
-        <img src="./assets/img/<?php echo $acts['afbeelding'];?>_100.jpg"
-        alt="<?php echo $acts["afbeelding"];?>" />
-          <!-- <a class='act__link' href="index.php?page=detail&amp;id=<?php echo $acts["act_id"];?>"> -->
-          <!-- <picture class="act__image">
-              <source
-              type="image/webp"
-              srcset="
-              assets/img/<?php echo $acts["afbeelding"];?>_10.webp 155w,
-              assets/img/<?php echo $acts["afbeelding"];?>_20.webp 310w,
-              assets/img/<?php echo $acts["afbeelding"];?>_33.webp 517w,
-              assets/img/<?php echo $acts["afbeelding"];?>_50.webp 776w,
-              assets/img/<?php echo $acts["afbeelding"];?>_67.webp 1034w,
-              assets/img/<?php echo $acts["afbeelding"];?>_75.webp 1164w,
-              assets/img/<?php echo $acts["afbeelding"];?>_85.webp 1319w,
-              assets/img/<?php echo $acts["afbeelding"];?>_100.webp 1552w
-              "
-              sizes="(min-width: 1440px) 59rem,
-              (min-width: 1024px) 47rem,
-              (min-width: 768px) 36rem,
-              (min-width: 320px) 54rem,
-              (min-width: 0) 100vw
-              "
-              />
-              <source
-              type="image/jpg"
-              srcset="
-              assets/img/<?php echo $acts["afbeelding"];?>_10.jpg 155w,
-              assets/img/<?php echo $acts["afbeelding"];?>_20.jpg 310w,
-              assets/img/<?php echo $acts["afbeelding"];?>_33.jpg 517w,
-              assets/img/<?php echo $acts["afbeelding"];?>_50.jpg 776w,
-              assets/img/<?php echo $acts["afbeelding"];?>_67.jpg 1034w,
-              assets/img/<?php echo $acts["afbeelding"];?>_75.jpg 1164w,
-              assets/img/<?php echo $acts["afbeelding"];?>_85.jpg 1319w,
-              assets/img/<?php echo $acts["afbeelding"];?>_100.jpg 1552w
-              "
-              sizes="(min-width: 1440px) 59rem,
-              (min-width: 1024px) 47rem,
-              (min-width: 768px) 36rem,
-              (min-width: 320px) 54rem,
-              (min-width: 0) 100vw
-              "
-              /> -->
-            <!-- </picture> -->
-            <!-- </a> -->
-          </li>
-    </ul>
-  <!-- </div> -->
+  <ul class="content">
+    <li class="content__li"><img src="./assets/img/<?php echo $acts['afbeelding'];?>_50.jpg" alt="<?php echo $acts["afbeelding"];?>"/></li>
+  </ul>
 </article>
 
 <article class="familiedag">
-  <div>
+  <div class="familiedag__wrapper">
     <h3 class="heading-3">Familiedag</h3>
     <p>Ook dit jaar geniet je op zaterdag opnieuw van vaste voorstellingen gericht op de hele familie. Wij voorzien kinderkoptelefoons en voor de allerkleinste oorkappen. Mis dit niet en beleef dit avontuur samen!</p>
     <a class="cta" href="index.php?page=programma">Programma</a>
   </div>
-  <img src="" alt="">
+  <ul class="familiedag__ul">
+    <li><img src="./assets/img/familie/1_20.jpg" alt="sfeerfoto 1 familiedag" width="150" height="150"></li>
+    <li><img src="./assets/img/familie/2_20.jpg" alt="sfeerfoto 2 familiedag" width="150" height="150"></li>
+    <li><img src="./assets/img/familie/3_20.jpg" alt="sfeerfoto 3 familiedag" width="150" height="150"></li>
+    <li><img src="./assets/img/familie/4_20.jpg" alt="sfeerfoto 4 familiedag" width="150" height="150"></li>
+  </ul>
 </article>
 
 <article class="praktisch">
   <h3 class="heading-3 hidden">Praktisch</h3>
- <img src="./assets/img/plattegrond/algemeen.png" alt="kaart van ISB">
+ <img src="./assets/img/plattegrond/algemeen_50.png" alt="kaart van ISB">
 </article>
 
 <article class="nieuwsbrief">
