0671b6ab1bc3c4bda032a6dfdd21c37b020aa31e
Wencke <wencke.nuyt@student.howest.be>
2019-01-26 22:31:49 +0100

    hom

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -1,17 +1,17 @@
 <section class="home">
 <header class="home__header">
   <h2 class="heading-1 hidden">Home</h2>
-  <a class="header__cta" href="#spektakel"><span class="header__hidden">Ontdek</span></a>
-  <a class="header__cta__mobile" href="index.php?page=programma"><span class="header__mobile">Ontdek het programma</span></a>
+  <a class="header__cta body__home" href="#spektakel"><span class="header__hidden">Ontdek</span></a>
+  <a class="header__cta__mobile body__home" href="index.php?page=programma"><span class="header__mobile">Ontdek het programma</span></a>
 </header>
 
 <article class="spektakel container" id="spektakel">
   <div class="spektakel__groep">
     <div>
-      <h3 class="heading-2">Een onvergetelijk spektakel</h3>
-      <p class="cta"><a class=" underline--magical cta__link" href="index.php?page=programma">Programma</a></p>
+      <h3 class="heading-2 titel__home">Een onvergetelijk spektakel</h3>
+      <p class="cta hide__mobile body__home"><a class="underline--magical cta__link" href="index.php?page=programma">Programma</a></p>
     </div>
-    <p class="spektakel__lijn">Humor, acrobatie, stuntwerk van de bovenste plank uit ruim <span class="strong">15 verschillende </span>landen! Bovenal geniet u van dit festival volledig <span class="strong">GRATIS</span>.</p>
+    <p class="spektakel__lijn body__home">Humor, acrobatie, stuntwerk van de bovenste plank uit ruim <span class="strong">15 verschillende </span>landen! Bovenal geniet u van dit festival volledig <span class="strong">GRATIS</span>.</p>
   </div>
   <div class="spektakel__content">
     <?php
@@ -68,9 +68,9 @@
 <article class="familiedag">
   <div class="container home__familiedag">
   <div class="familiedag__info">
-    <h3 class="heading-2 familiedag__lijn">Familiedag</h3>
-    <p class="familiedag__tekst">Ook dit jaar geniet je op zaterdag opnieuw van vaste voorstellingen gericht op de hele familie. Wij voorzien kinderkoptelefoons en voor de allerkleinste oorkappen. Mis dit niet en beleef dit avontuur samen!</p>
-    <p class="cta familiedag__cta"><a class="underline--magical cta__link" href="index.php?page=programma">Programma</a></p>
+    <h3 class="heading-2 familiedag__lijn titel__home">Familiedag</h3>
+    <p class="familiedag__tekst body__home">Ook dit jaar geniet je op zaterdag opnieuw van vaste voorstellingen gericht op de hele familie. Wij voorzien <span class="strong">kinderkoptelefoons</span> en voor de allerkleinste oorkappen. Mis dit niet en beleef dit avontuur samen!</p>
+    <p class="cta body__home familiedag__cta"><a class="underline--magical cta__link" href="index.php?page=programma">Programma</a></p>
   </div>
   <div class="familiedag__img">
     <picture class="familiedag__image">
@@ -114,16 +114,16 @@
   </div>
 </article>
 
-<article class="nieuwsbrief container">
-  <h3 class="heading-1 hidden">Nieuwsbrief</h3>
+<article class="nieuwsbrief ">
+  <h3 class="heading-1 nieuwsbrief__titel"><span class="hidden">Nieuwsbrief</span></h3>
   <!-- <article class="aftermovie">
     <h4 class="hidden">Dit was 2017</h4>
     <iframe class = "aftermovie__beeld"
     src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fstraattheaterfestivalbeveren%2Fvideos%2F1949847618626827%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
   </article> -->
-  <article class="nieuwsbrief__article">
-    <h4 class="heading-2">Wil je op de hoogte blijven van onze laatste nieuwtjes?</h4>
-    <p class='nieuwsbrief__tekst'>Schrijf je dan in op onze nieuwsbrief.</p>
+  <article class="nieuwsbrief__article container">
+    <h4 class="heading-2 titel__home">Wil je op de hoogte blijven van onze laatste nieuwtjes?</h4>
+    <p class='nieuwsbrief__tekst body__home'>Schrijf je dan in op onze nieuwsbrief.</p>
     <form action="" class='nieuwsbrief__form'>
       <input type="hidden" action="" value="">
       <label for="email" class="email__label">e-mailadres</label>
@@ -150,7 +150,7 @@
       (min-width: 1024px) 105vw,
       (min-width: 768px) 105vw,
       (min-width: 320px) 105vw,
-      (min-width: 0) 100vw
+      (min-width: 0) 105vw
       "
       />
       <source
